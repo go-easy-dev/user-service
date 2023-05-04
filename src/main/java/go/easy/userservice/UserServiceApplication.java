8299429b8197bed7faae4f11678730b530bf921a
sakhno <sakhno@pulse.insure>
2023-05-04 19:16:15 +0300

    init commit

---
+++ src/main/java/go/easy/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package go.easy.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/go/easy/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package go.easy.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
