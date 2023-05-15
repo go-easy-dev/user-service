e88033ba34210b71a6b014ad4ffac0b7d99be771
sakhno <sakhno@pulse.insure>
2023-05-15 16:53:48 +0300

    fixes

--- src/main/java/go/easy/userservice/dto/CreateUserRequest.java
+++ src/main/java/go/easy/userservice/dto/CreateUserRequest.java
@@ -1,13 +1,17 @@
 package go.easy.userservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
 public class CreateUserRequest {
 
-    @NotBlank String firstName;
-    @NotBlank String email;
-    @NotBlank Gender gender;
+    @NotBlank
+    String firstName;
+    @NotBlank
+    String email;
+    @NotNull
+    Gender gender;
 
 }
